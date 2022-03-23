d4ef0356605b75cb113a81c1c703a84306d60d82
Appazov <anvar978@yandex.ru>
2022-03-23 23:09:30 +0400

    Add login

--- authController.js
+++ authController.js
@@ -1,7 +1,17 @@
 const User = require('./models/User')
 const Role = require('./models/Role')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const { validationResult } = require('express-validator')
+const { secret } = require('./config')
+
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles,
+  }
+  return jwt.sign(payload, secret, { expiresIn: '24h' })
+}
 
 class AuthController {
   async registration(req, res) {
@@ -27,6 +37,17 @@ class AuthController {
   }
   async login(req, res) {
     try {
+      const { username, password } = req.body
+      const user = await User.findOne({ username })
+      if (!user) {
+        return res.status(400).json({ message: 'User does not exist' })
+      }
+      const validPassword = bcrypt.compareSync(password, user.password)
+      if (!validPassword) {
+        return res.status(400).json({ message: 'Wrong password' })
+      }
+      const token = generateAccessToken(user._id, user.roles)
+      return res.json(token)
     } catch (e) {
       console.log(e)
       res.status(400).json({ message: 'Login error' })
@@ -34,7 +55,8 @@ class AuthController {
   }
   async getUsers(req, res) {
     try {
-      res.json('server is working')
+      const users = await User.find()
+      res.json(users)
     } catch (e) {
       console.log(e)
     }
