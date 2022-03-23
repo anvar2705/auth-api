8e8151a6fa0ebe0d10bbed651678fe6010674c32
Appazov <anvar978@yandex.ru>
2022-03-23 21:52:02 +0400

    Add authRouter

---
+++ authController.js
@@ -0,0 +1,17 @@
+class AuthController {
+  async registration(req, res) {
+    try {
+    } catch (error) {}
+  }
+  async login(req, res) {
+    try {
+    } catch (error) {}
+  }
+  async getUsers(req, res) {
+    try {
+      res.json('server is working')
+    } catch (error) {}
+  }
+}
+
+module.exports = new AuthController()
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const authRouter = require('./authRouter')
+const PORT = process.env.PORT || 4000
+
+const app = express()
+
+app.use(express.json())
+app.use('/auth', authRouter)
+
+const start = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://anvar978:1PH4qAnwBNQmpxe5@cluster0.bzyht.mongodb.net/auth-api-DB?retryWrites=true&w=majority`
+    )
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
