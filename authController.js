513b2b986d8babf0a6433cee9c50cf0f3e24b3d3
Appazov <anvar978@yandex.ru>
2022-03-23 22:36:42 +0400

    Add registration

--- authController.js
+++ authController.js
@@ -1,16 +1,43 @@
+const User = require('./models/User')
+const Role = require('./models/Role')
+const bcrypt = require('bcryptjs')
+const { validationResult } = require('express-validator')
+
 class AuthController {
   async registration(req, res) {
     try {
-    } catch (error) {}
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: 'Registration error', errors })
+      }
+      const { username, password } = req.body
+      const candidate = await User.findOne({ username })
+      if (candidate) {
+        return res.status(400).json({ message: 'This user already exists' })
+      }
+      const hashPassword = bcrypt.hashSync(password, 7)
+      const userRole = await Role.findOne({ value: 'USER' })
+      const user = new User({ username, password: hashPassword, roles: [userRole.value] })
+      await user.save()
+      return res.json({ message: 'User successfully registered' })
+    } catch (e) {
+      console.log(e)
+      res.status(400).json({ message: 'Registration error' })
+    }
   }
   async login(req, res) {
     try {
-    } catch (error) {}
+    } catch (e) {
+      console.log(e)
+      res.status(400).json({ message: 'Login error' })
+    }
   }
   async getUsers(req, res) {
     try {
       res.json('server is working')
-    } catch (error) {}
+    } catch (e) {
+      console.log(e)
+    }
   }
 }
 
--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,18 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('./authController')
+const { check } = require('express-validator')
 
-router.post('/registration', controller.registration)
+router.post(
+  '/registration',
+  [
+    check('username', 'Username must not be empty or contain spaces')
+      .notEmpty()
+      .custom((value) => !/\s/.test(value)),
+    check('password', 'Password length must be more than 6 characters').isLength({ min: 6 }),
+  ],
+  controller.registration
+)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
 
